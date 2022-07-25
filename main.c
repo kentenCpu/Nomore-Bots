2b3b8078aef770e945296d463d18f6330294070c
kentenCpu <96505472+kentenCpu@users.noreply.github.com>
2022-07-25 19:07:14 -0400

    this is the first upload and not playable

---
+++ main.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <string.h>
+int main(){printf("Nomore Bots")};return 0;}
+char name[]="Delfor"; char []class="Mage";
+char opener[]="im going in.."; 
+int power=10;
+
+char potato_bot[]="Potatoez";
+char potato_bot_desc[]="He's known to have 5 computers but it's not certain";
+char potato_bot_defeatline[]="nooooo..";
+char potato_bot_power=1;
+
+char levelone[]="Sector55";
+char levelone_story_line_1[]="I want you to track down a bot known as Islpotoato. This bot is complicated, as he is equiped with a speakermod system which"
+char levelone_story_line_2[]="As you enter Sector55, you realize two passageways, one is dark while the other is well lit.  You cannot hear any sound 
+
+
+
