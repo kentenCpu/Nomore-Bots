542adad9088d66d2d7882d265ee6807d0751b82b
kentenCpu <96505472+kentenCpu@users.noreply.github.com>
2022-07-26 13:07:06 -0400

    third upload

--- main.c
+++ main.c
@@ -1,51 +1,60 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 int main(){
 
 printf("Nomore Bots\n");
 printf("-----------\n");
-
 char name[]="Delfor";
-char proffesion[]="Tactician"; 
-int power=10;
+char proffesion[]="Tactician";
 
 char potato_bot[]="Potatoez";
-char potato_bot_desc[]="He's known to pocess 5 computers but this is not certain";
-char potato_bot_defeatline[]="nooooo..";
-char potato_bot_power=1;
+char potato_bot_desc[]="He's known to possess 5 computers but this is not certain";
+char potato_bot_defeatline[]="Nooooo..";
+int potato_bot_power=1;
 
-char levelone[]="Sector55";
-char levelone_story_line_1[]="I want you to track down a bot known as Islpotoato. This bot is complicated, as he is equiped with a speakermod system which projects sounds as well as radiation.\n";
-char levelone_story_line_2[]="Good, you've entered the sector.  Yes, this is Sector55, you'll realize two passageways.  The one to your right is dark as you can tell.  Take that one, and not the well lit room.\n";
+char levelone[]="Sector 55";
+//char levelone_story_line_1_1[]="[Newtab] > This bot is rather complicated, as it's equiped with a speakermod system which can project sounds as well as radiation.""\n\n";
+//char levelone_story_line_2[]="[Newtab] > ""Good, you've entered the sector.\n\n";
 
-char opener_sector55_1[]="You are assigned by newtab to investigate Sector55.  From the crowded city you enter a warehouse.\n";
-char opener_sector55_2[]="...\n"; 
-char opener_sector55_3[]="[newtab]> Good, you've made it. We're running out of time with this one.\n";
-char opener_sector55_4[]="He's known as potatoez.  Reports indicates he's planning to create more bots that we need to take out\n";
+int yes_no(){char response; scanf("%c", "%response"); if(response=='y'){return 1;} return 0;}
+void nl(){printf("\n");}
 
-char opener_sector55_ready[]="Are you ready? (y/n or y/anykey)\n";
+printf("You are assigned by newtab to investigate Sector55.\n\n From the crowded city you enter a warehouse.\n\n");sleep(6);
+printf("'Newtab' will guide you via microphone through the mission.\n\n"); sleep(5);
+printf("...\n\n"); sleep(5);
+printf("Newtab(talking) > Good, you've made it. We're running out of time and we must be careful.\n\n"); sleep(6);
+printf("Our target today is one of these 'botter's, he's known as potatoez.  Our intelligence indicates that he's planning on creating more bots here in Sector 55.\n\n");sleep(6);
+printf("Are you ready? (y/n or y/anykey)\n\n");
+while(1 == 1){
+char response; scanf(" %c", &response); if(response != 'y') {
+printf("\n[Newtab] > Okay, just let me know when you're ready.\n\n");
+printf("Are you ready? (y/n or y/anykey)\n\n"); }
+else if(response == 'y'){break;}}
 
-int yes_no(){char response; scanf("%c", "%response"); if(response=='y'||response=='Y'){return 1;} return 0;}
+//opener_sector55_start_1();
+sleep(1);
+printf("\n[Newtab] > ""I want you to track down one of Potatoez bot's known as 'Islpotoato'.\n\n"); sleep(6);
+printf("[Newtab] > This bot is rather complicated, as it's equiped with a speakermod system which can project sounds as well as radiation.\n\n"); sleep(8);
+printf("[Newtab] > ""Good, you've entered the sector.\n\n"); sleep(5);
 
-printf("%s", opener_sector55_1);
-printf("%s", opener_sector55_2);
-printf("%s", opener_sector55_3);
-printf("%s", opener_sector55_4);
-printf("%s", opener_sector55_ready);
-
-int opener_sector55_start_1(){
-while(1==1){if (yes_no()==0){printf("okay, just let me know when you're ready.\n");
-printf("%s", opener_sector55_ready);
+printf("[Newtab] > Yes, this is Sector 55, you'll realize multiple passageways before you.\n\n");sleep(6);
+printf("[Newtab] > The one to your right will lead you to the second corridor.\n\n");sleep(6);
+//printf("...\n\n");sleep(5);
+printf("[Newtab] > Take that passageway."".\n\n");sleep(4);
+//mission1
+printf("? Take the right passageway? (y/n or anykey)\n\n");
+while(1==1){char r; scanf(" %c", &r);
+if(r!='y'){printf("\nYou see no other option.\n\n");}else{break;}}
+//continue
+printf("* You gain 100 Experience Points.\n\n");sleep(2);
+printf(" You enter the passageway.\n\n");sleep(4);
+printf(" You see a computer panel along the wall. The device is turned on but you can't make out the interface.\n\n");sleep(8);
+printf(" You approach the device.\n\n"); sleep(6);
+printf("[Newtab] > I see you've found a com-link device.  Let me look up it's network protocol.\n\n"); sleep(6);
+printf(" ");sleep(5);
+printf("[Newtab] > Ahead of you and then to your right will be a sealed door.\n\n");sleep(6);
+printf("[Newtab] > The device in front of you can be used to unlock the door's security interface.\n\n"); sleep(8);
+printf("[Newtab] > Use the device.\n\n");sleep(4);
+printf("end-of-file");
 }
-else{return 0;}
-return 1;
-}}
-
-opener_sector55_start_1();
-
-printf("ok");
-
-}
-
-
-
