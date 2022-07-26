2b734ce7f73812bbfcca6f73bd3bfcbbfbfd58ab
kentenCpu <96505472+kentenCpu@users.noreply.github.com>
2022-07-26 19:23:42 -0400

    update

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <string.h>
 #include <unistd.h>
 int main(){
-
+printf("-----------\n");
 printf("Nomore Bots\n");
 printf("-----------\n");
+
 char name[]="Delfor";
 char proffesion[]="Tactician";
 
@@ -16,11 +17,12 @@ int potato_bot_power=1;
 char levelone[]="Sector 55";
 //char levelone_story_line_1_1[]="[Newtab] > This bot is rather complicated, as it's equiped with a speakermod system which can project sounds as well as radiation.""\n\n";
 //char levelone_story_line_2[]="[Newtab] > ""Good, you've entered the sector.\n\n";
-
-int yes_no(){char response; scanf("%c", "%response"); if(response=='y'){return 1;} return 0;}
+//int yes_no(){char response; scanf("%c", "%response"); if(response=='y'){return 1;} return 0;}
 void nl(){printf("\n");}
 
-printf("You are assigned by newtab to investigate Sector55.\n\n From the crowded city you enter a warehouse.\n\n");sleep(6);
+printf("You are assigned by newtab to investigate Sector 55.\n\n");sleep(6);
+printf("The city is in fear as those known as 'botter's emerged from hiding.  These 'botter's plan to take over the city.  Their practice of deploying 'bots' must be stopped.\n\n");sleep(8);
+printf("\n\n  ..From the crowded city you enter a warehouse.\n\n");sleep(6);
 printf("'Newtab' will guide you via microphone through the mission.\n\n"); sleep(5);
 printf("...\n\n"); sleep(5);
 printf("Newtab(talking) > Good, you've made it. We're running out of time and we must be careful.\n\n"); sleep(6);
@@ -46,15 +48,23 @@ printf("[Newtab] > Take that passageway."".\n\n");sleep(4);
 printf("? Take the right passageway? (y/n or anykey)\n\n");
 while(1==1){char r; scanf(" %c", &r);
 if(r!='y'){printf("\nYou see no other option.\n\n");}else{break;}}
+printf("\n");
 //continue
-printf("* You gain 100 Experience Points.\n\n");sleep(2);
-printf(" You enter the passageway.\n\n");sleep(4);
+//printf("* You gain 100 Experience Points.\n\n");sleep(2);
+printf(" You enter the passageway.\n\n");sleep(2);
+printf("* You gain 100 Experience Points.\n\n");sleep(4);
 printf(" You see a computer panel along the wall. The device is turned on but you can't make out the interface.\n\n");sleep(8);
-printf(" You approach the device.\n\n"); sleep(6);
-printf("[Newtab] > I see you've found a com-link device.  Let me look up it's network protocol.\n\n"); sleep(6);
-printf(" ");sleep(5);
+//printf(" You approach the device.\n\n"); sleep(6);
+printf("[Newtab] > I see you've found a com-link device.  Let me look up it's network protocol.\n\n"); sleep(10);
+//sleep(5);
 printf("[Newtab] > Ahead of you and then to your right will be a sealed door.\n\n");sleep(6);
 printf("[Newtab] > The device in front of you can be used to unlock the door's security interface.\n\n"); sleep(8);
 printf("[Newtab] > Use the device.\n\n");sleep(4);
+printf("");
+
+
+
+
+
 printf("end-of-file");
 }
